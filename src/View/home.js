69282955c27188d5c0fdef73a01eb30c52665f8e
Aziz Rahman <theazizstark@gmail.com>
2020-07-08 22:57:56 +0530

    Update home.js

--- src/View/home.js
+++ src/View/home.js
@@ -203,7 +203,7 @@ export default class Home extends Component {
         </section>
 
 
-        {herobar('Who am I?', 'About me')}
+        {herobar('Who am I?', 'I am Aziz')}
         <section id="about" className={`hero is-fullheight ${cstyles.svgg}`} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
           <div className="columns is-desktop" style={{ padding: '10%' }}>
             <div className="column  has-text-centered" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
